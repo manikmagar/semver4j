ec2826d8fc25dfa8e8651dc4c3e2a1e5924f2b67
Manik Magar <manik.magar@gmail.com>
2022-12-04 13:46:12 -0500

    feat: Create SemVer Core version with prerelese

---
+++ src/main/java/com/github/manikmagar/semver4j/SemVer.java
@@ -0,0 +1,133 @@
+/*
+ *  Copyright 2022 Manik Magar
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.github.manikmagar.semver4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import com.github.manikmagar.semver4j.internal.Util;
+
+import static com.github.manikmagar.semver4j.SemVer.Patterns.PRE_RELEASE;
+
+/**
+ * Create a valid Semantic Version.
+ *
+ * See <a href="https://semver.org/">SemVer.org</a> for specification details.
+ *
+ * @author manikmagar
+ */
+public class SemVer {
+	private AtomicInteger major = new AtomicInteger(0);
+	private AtomicInteger minor = new AtomicInteger(0);
+	private AtomicInteger patch = new AtomicInteger(0);
+	private final List<String> releaseIdentifiers = Collections.synchronizedList(new ArrayList<>());
+
+	public static final SemVer ZERO = new SemVer(0, 0, 0);
+
+	/**
+	 * Create a SemVer for given version components
+	 * 
+	 * @param major
+	 *            int
+	 * @param minor
+	 *            int
+	 * @param patch
+	 *            int
+	 */
+	public SemVer(int major, int minor, int patch) {
+		Util.mustBePositive(major);
+		Util.mustBePositive(minor);
+		Util.mustBePositive(patch);
+		this.major = new AtomicInteger(major);
+		this.minor = new AtomicInteger(minor);
+		this.patch = new AtomicInteger(patch);
+	}
+
+	/**
+	 * A default SemVer 0.0.0
+	 */
+	public SemVer() {
+		this(0, 0, 0);
+	}
+
+	public int getMajor() {
+		return major.get();
+	}
+
+	public int getMinor() {
+		return minor.get();
+	}
+
+	public int getPatch() {
+		return patch.get();
+	}
+
+	public void validate() {
+		Util.mustBePositive(getMajor());
+		Util.mustBePositive(getMinor());
+		Util.mustBePositive(getPatch());
+	}
+
+	public static SemVer with(int major, int minor, int patch) {
+		return new SemVer(major, minor, patch);
+	}
+
+	public SemVer withReleaseIdentifier(String identifier) {
+		Objects.requireNonNull(identifier, "Identifier must not be null");
+		if (!PRE_RELEASE.matcher(identifier).matches()) {
+			throw new IllegalArgumentException(String.format("Release identifier '%s' does not match with pattern '%s'",
+					identifier, PRE_RELEASE.pattern()));
+		}
+		this.releaseIdentifiers.add(identifier);
+		return this;
+	}
+
+	public SemVer incrementMajor() {
+		major.incrementAndGet();
+		minor.set(0);
+		patch.set(0);
+		return this;
+	}
+
+	public SemVer incrementMinor() {
+		minor.incrementAndGet();
+		patch.set(0);
+		return this;
+	}
+
+	public SemVer incrementPatch() {
+		patch.incrementAndGet();
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		String version = String.format("%d.%d.%d", getMajor(), getMinor(), getPatch());
+		String releaseIdentifier = releaseIdentifiers.stream().collect(Collectors.joining("."));
+		if (!releaseIdentifier.isEmpty())
+			version = version + "-" + releaseIdentifier;
+		return version;
+	}
+
+	public interface Patterns {
+		Pattern PRE_RELEASE = Pattern.compile("^[0-9A-Za-z-.]*$");
+	}
+}
---
+++ src/test/java/com/github/manikmagar/semver4j/SemVerTest.java
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2022 Manik Magar
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.github.manikmagar.semver4j;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+
+class SemVerTest {
+
+	@Test
+	@DisplayName("Default SemVer version value")
+	void defaultVersion() {
+		assertThat(new SemVer()).as("Semver Default").extracting(SemVer::getMajor, SemVer::getMinor, SemVer::getPatch)
+				.containsExactly(0, 0, 0);
+	}
+
+	@Test
+	@DisplayName("Core version components extracted")
+	void coreSemVerComponents() {
+		assertThat(new SemVer(1, 2, 3)).as("Semver 1.2.3")
+				.extracting(SemVer::getMajor, SemVer::getMinor, SemVer::getPatch).containsExactly(1, 2, 3);
+	}
+
+	@Test
+	@DisplayName("Core version to String")
+	void coreSemVerToString() {
+		assertThat(new SemVer(1, 2, 3)).as("Semver 1.2.3").asString().isEqualTo("1.2.3");
+	}
+
+	@ParameterizedTest
+	@CsvSource({"-1,1,1,-1", "2,-2,2,-2", "3,3,-3,-3"})
+	@DisplayName("Negative version component value")
+	void negativeVersionTest(int major, int minor, int patch, int negativeValue) {
+		IllegalArgumentException exception = Assertions.catchThrowableOfType(() -> new SemVer(major, minor, patch),
+				IllegalArgumentException.class);
+		assertThat(exception).isNotNull().hasMessage(String.format("Number %s must be positive", negativeValue));
+	}
+
+	@Test
+	void validate() {
+	}
+
+	@Test
+	void withVersions() {
+		assertThat(SemVer.with(1, 2, 3)).as("Semver 1.2.3").asString().isEqualTo("1.2.3");
+	}
+
+	@Test
+	@DisplayName("Increment Major version")
+	void incrementMajor() {
+		assertThat(new SemVer(1, 2, 3).incrementMajor()).as("Semver 1.2.3 incremented major").asString()
+				.isEqualTo("2.0.0");
+	}
+
+	@Test
+	@DisplayName("Increment Minor version")
+	void incrementMinor() {
+		assertThat(new SemVer(1, 2, 3).incrementMinor()).as("Semver 1.2.3 incremented minor").asString()
+				.isEqualTo("1.3.0");
+	}
+
+	@Test
+	@DisplayName("Increment Patch version")
+	void incrementPatch() {
+		assertThat(new SemVer(1, 2, 3).incrementPatch()).as("Semver 1.2.3 incremented patch").asString()
+				.isEqualTo("1.2.4");
+	}
+
+	@Test
+	@DisplayName("Single prerelease identifier with allowed characters")
+	void withSinglePrereleaseIdentifier() {
+		assertThat(new SemVer(1, 2, 3).withReleaseIdentifier("Al-pha01"))
+				.as("Prerelease Version with all allowed characters").asString().isEqualTo("1.2.3-Al-pha01");
+	}
+
+	@Test
+	@DisplayName("Single prerelease identifier with disallowed characters")
+	void withPrereleaseInvalidIdentifier() {
+		IllegalArgumentException exception = catchThrowableOfType(
+				() -> new SemVer(1, 2, 3).withReleaseIdentifier("Al-pha01#"), IllegalArgumentException.class);
+		assertThat(exception).isNotNull()
+				.hasMessage("Release identifier 'Al-pha01#' does not match with pattern '^[0-9A-Za-z-.]*$'");
+	}
+
+	@Test
+	@DisplayName("Multiple prerelease identifiers")
+	void withMultiplePrereleaseIdentifier() {
+		assertThat(new SemVer(1, 2, 3).withReleaseIdentifier("alpha").withReleaseIdentifier("1")
+				.withReleaseIdentifier("2")).as("Prerelease Version").asString().isEqualTo("1.2.3-alpha.1.2");
+	}
+
+}
